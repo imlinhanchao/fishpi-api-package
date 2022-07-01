e7b48f5897a366fd4f2dceddf79e2d92d43a868d
Hancel.Lin <imlinhanchao@gmail.com>
2022-07-02 00:04:54 +0800

    添加私聊对象

--- src/typing.d.ts
+++ src/typing.d.ts
@@ -1920,3 +1920,57 @@ export type CommentPost = {
      */
     commentOriginalCommentId?: string,
 }
+
+/**
+ * 私聊内容
+ */
+export type PrivateChat = {
+    /**
+     * 接收者 Id
+     */
+    toId: string,
+    /**
+     * 预览内容
+     */
+    preview: string,
+    /**
+     * 消息用户 Session
+     */
+    user_session: string,
+    /**
+     * 发送者头像
+     */
+    senderAvatar: string,
+    /**
+     * 消息 Markdown
+     */
+    markdown: string,
+    /**
+     * 接收者头像
+     */
+    receiverAvatar: string,
+    /**
+     * 消息 Id
+     */
+    oId: string,
+    /**
+     * 发送时间
+     */
+    time: string,
+    /**
+     * 来源 Id
+     */
+    fromId: string,
+    /**
+     * 发送者用户名
+     */
+    senderUserName: string,
+    /**
+     * 消息内容 HTML
+     */
+    content: string,
+    /**
+     * 接收者用户名
+     */
+    receiverUserName: string
+}
